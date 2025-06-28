992ee5e911d95a416a5fab49fe13ebaa552e410a
Cole Naomi Oritsemeyiwa <runocole@gmail.com>
2025-06-28 16:09:19 +0100

    Switch Render backend back to SQLite for frontend testing

--- easypass/settings.py
+++ easypass/settings.py
@@ -58,11 +58,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'easypass.wsgi.application'
 
 # Database
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
