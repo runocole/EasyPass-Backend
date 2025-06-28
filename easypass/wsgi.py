eeca1823f3981c093b5daf39ee34628bab978d88
Cole Naomi Oritsemeyiwa <runocole@gmail.com>
2025-06-28 16:18:11 +0100

    Add auto superuser creation for initial SQLite setup

--- easypass/wsgi.py
+++ easypass/wsgi.py
@@ -13,4 +13,16 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easypass.settings")
 
+
+import django
+from django.core.management import call_command
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easypass.settings')
+django.setup()
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "adminpassword")
+
 application = get_wsgi_application()
